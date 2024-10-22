7aeae305864ac229509c581f67005b6506b4f8ee
elipedro77 <elipderope@gmail.com>
2024-10-22 09:56:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Quantos anos tem neymar ?',
+    '32'
+)
+
+criaCartao(
+    'Futebol',
+    'Em que time Neymar Jr. esta jogando atualmente ',
+    'Al-Hilal'
+)
+
+criaCartao(
+    'Futebol',
+    'quantos gols neymar tem em sua carreira ',
+    '439'
+)
+
+criaCartao(
+    'futebol',
+    'qual o time que revelou neymar ?',
+    'Santos futebol clube'
+)
